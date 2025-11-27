addf79ff69a2d0556c21e97687555c941a6079f4
Egor Mamonov <egormamonov@gmail.com>
2025-11-27 11:48:43 +0100

    Fix menu icon and name in tab

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Sensor Monitoring Dashboard';
+  title = 'Sensor Monitoring';
 }
